aa725a16bd94c6950060528e95cca5aabb7d43ee
Kozyavin V <kioci@yandex.ru>
2023-08-14 00:03:09 +0300

    PostmanErrorSomeDataNotSomeValue

---
+++ src/test/java/ru/netology/MobileBankApiTestV7.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+public class MobileBankApiTestV7 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+    }
+}
